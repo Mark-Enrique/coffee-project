50b838d35895ddd75d95eb7130836f31858ade18
Mark Stachowski <markstachowski@me.com>
2018-10-17 11:04:01 -0500

    mark testbranch push 1-before switching

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function renderCoffee(coffee) {
+var renderCoffee = function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
@@ -10,7 +10,7 @@ function renderCoffee(coffee) {
     return html;
 }
 
-function renderCoffees(coffees) {
+var renderCoffees = function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
@@ -18,7 +18,7 @@ function renderCoffees(coffees) {
     return html;
 }
 
-function updateCoffees(e) {
+var updateCoffees = function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
